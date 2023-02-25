422ece2dd020d0a610450f8b5ddc1d369743b7ee
johanrizky <rizkyt64@gmail.com>
2023-02-25 21:46:00 +0700

    dart packages library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages/hello.dart';
+
+void main() {
+  print(sayHello("Johan"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library dart_packages;
+
+export 'src/say_hello.dart';
+export 'src/customer.dart' show Category, Product, Customer;
